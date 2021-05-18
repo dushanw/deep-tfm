d87c7f6b7293f28dbccbe2cdfe0cdeb5dfe833ad
Dushan N. Wadduwage <15632687+dushanw@users.noreply.github.com>
2021-05-18 11:11:01 +0530

    clean ups to main.m

--- main.m
+++ main.m
@@ -1,29 +1,33 @@
-% 20200909 by Dushan N Wadduwage (DNW)
+% 20200909 by Dushan N Wadduwage (wadduwage@fas.harvard.edu)
 % 20201222 modified by DNW
-% test for real data
+% 20210518 modified by DNW
+
+%% Notes and Requirements 
+% (1).This code require CVX for optimization (http://cvxr.com/cvx/)
+% (2).Beware that large-sized image reconstruction might be memory and time intensive.
+% (3).To adapt to new data please update the sections "parameters" and "read and preprocess data"
+% (4).This is a legacy version of the code and will is not maintained.
+%     Please refer to https://github.com/dushanw/deep-tfm-vX.X 
+%     (vX.X is the version, eg. the current version is v1.0) for the latest version.
 
 %% system setup
 clc; clear all; close all
 
-addpath(genpath('./_functionsAndLayers/'))
-addpath('./_Datasets/')
-addpath('./_ExtPatternsets/')
-
-% setup cvx <cpy cvx here or give it as requirement for the code>
-run('../_toolkits/cvx/cvx_startup.m')
+% setup cvx
+% run('../../_toolkits/cvx/cvx_startup.m')
 
 %% parameters
-pram.dx0     = 0.33;                % original pixel size of image before resizeing to Nx Ny
-pram.Nx      = 16;
-pram.Ny      = 16;
-pram.Nt      = 16;
-gamma        = 5e-4;
-wname        = 'db4';
-savepath     = ['./__results/' date '_tfm_mouse_20201224/'];
-mkdir(savepath)
+pram.dx0          = 0.33;                 % original pixel size of image before resizeing to Nx Ny
+pram.Nx           = 128;                  % 128x128 take 40mins per image on a computer with 1TB memory
+pram.Ny           = 128;
+pram.Nt           = 240;                  % #patterns
+pram.gamma        = 5e-4;                 % weight of the wavelet sparsity prior in the optimization function
+pram.wname        = 'db4';                % wavelet family used
 
-fileNameStem = sprintf('rec_Ny%d_Nx%d_Nt%d',pram.Ny,pram.Nx,pram.Nt);
-disp(fileNameStem)
+pram.fileNameStem = sprintf('rec_Ny%d_Nx%d_Nt%d',pram.Ny,pram.Nx,pram.Nt);
+pram.savepath     = ['./__results/' date '_deep-tfm_fig4/' pram.fileNameStem '/'];
+
+mkdir(pram.savepath)                      % results will be saved to this folder
 
 %% read and preprocess data
 load('./Data_fig4.mat');
@@ -53,11 +57,11 @@ Y_tfm   = Y_tfm  ./ max(max(Y_tfm,[],1),[],2);
 %% reconstruct and save results
 for i=1:size(Y_deep,4)
   i
-  Xhat_deep_noPr(:,:,:,i) = f_rec_inv_noPrior(pram,E,Y_deep(:,:,:,i),Y_tfm(:,:,:,i));  % no-prior 
-  Xhat_deep_wlPr(:,:,:,i) = f_rec_inv_wlPrior(pram,E,Y_deep(:,:,:,i),gamma,wname);     % wavelet-prior with cvx
+  Xhat_deep_noPr(:,:,:,i) = f_rec_inv_noPrior(pram,E,Y_deep(:,:,:,i),Y_tfm(:,:,:,i));         % no-prior 
+  Xhat_deep_wlPr(:,:,:,i) = f_rec_inv_wlPrior(pram,E,Y_deep(:,:,:,i),pram.gamma,pram.wname);  % wavelet-prior with cvx
 end
-save([savepath 'reconstructed_' fileNameStem '.mat'],'Xhat_deep_noPr','Xhat_deep_wlPr')
-%load([savepath 'reconstructed_' fileNameStem '.mat'])
+ save([pram.savepath 'reconstructed_' pram.fileNameStem '.mat'],'Xhat_deep_noPr','Xhat_deep_wlPr')
+%load([pram.savepath 'reconstructed_' pram.fileNameStem '.mat'])
 
 %% plot results
 for i=1:size(Y_deep,4)
@@ -68,6 +72,6 @@ for i=1:size(Y_deep,4)
   title('(1)Tfm-image (2)Deep-tfm-noPrior  (3)Deep-tfm-waveletPr')
   set(gca,'fontsize',24)
 
-  saveas(gcf,[savepath fileNameStem exp_names{i} '_fig.jpeg']);  
+  saveas(gcf,[pram.savepath exp_names{i} '_fig.jpeg']);  
   close all
 end
